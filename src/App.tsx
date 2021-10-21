a44d6efaeddadd0fa1c94c1d73a5821185da0fcb
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-21 18:01:00 -0300

    create Loginbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-export function App() {
+import styles from './App.module.scss';
 
+import { LoginBox } from "./components/LoginBox";
+import { MessageList } from "./components/MessageList";
+
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
   )
 }
