25a587bc42c1e3cea6aa0d02fffb7c30e1d4bb7d
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-22 20:48:48 -0300

    finish application

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -2,7 +2,7 @@ import logoImg from '../../assets/logo.svg';
 import { useEffect, useState } from 'react';
 
 import { api } from '../../services/api';
-
+import { io } from 'socket.io-client';
 import styles from './styles.module.scss';
 
 type Message = {
@@ -14,8 +14,29 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ].filter(Boolean))
+        messagesQueue.shift();
+      }
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('/messages/last3').then(response => {
